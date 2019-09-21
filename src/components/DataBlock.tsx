599fdb26eba1a49b3441571852152ca936709e55
Kenan Rustamov <kenanrustamov@gmail.com>
2019-09-21 12:17:19 -0400

    few more changes to datablocks for increased fluidity

--- src/components/DataBlock.tsx
+++ src/components/DataBlock.tsx
@@ -8,14 +8,16 @@ const DataBlock = (props: any) => {
             <div className="text">{props.propInput.title}</div>
             <div className="bottomBorder"></div>
         </div>
-        {props.propInput.infoTitle != null? <div className="info">
+        {props.propInput.ignoreInfo == null? 
+         <div className="info">
+         {props.propInput.infoTitle != null?
             <div className="infoTitle">{props.propInput.infoTitle}
                 <div className ="subtitle">{props.propInput.infoSubtitle}</div>
                 <div className="infoText">{props.propInput.infoText}</div>
-            </div>
+            </div>: null}
             {props.propInput.infoImg != null ? <img className = "image" src={props.propInput.infoImg} alt = {props.alt} ></img> : null}
             {props.propInput.other}
-        </div> : null}
+         </div> : null}
     </div>
     ;
     return content ;
--- src/components/DataBlocks.tsx
+++ src/components/DataBlocks.tsx
@@ -5,8 +5,9 @@ import kenanPhoto from "../images/kenanRustamov.jpg";
 import partifyGif from "../images/partify.png"
 import DashCardImg from "../images/DashCardImg.png";
 import BentleyPhoto from "../images/BentleyPhoto.jpeg";
-import PortfolioPic from "../images/PortfolioPic.png"
-import krLogo from "../images/krLogo.svg"
+import krLogo from "../images/krLogo.svg";
+import emailLogo from "../images/emailLogo.svg";
+
 
 const DataBlocks = (props: any) => {
     let content = 
@@ -16,10 +17,10 @@ const DataBlocks = (props: any) => {
         <DataBlock propInput ={{title: "Projects",infoTitle:"Partify", infoSubtitle:"React/TypeScript/Scss", infoText:"Partify is an app that allows you to use you and your friends' music tastes to create a custom tailored playlist for any occasion.", infoImg: partifyGif}}></DataBlock>
         <DataBlock propInput ={{title: "",infoTitle:"DashCard", infoSubtitle:"Java/XML/Android Studios/Room Persistence", infoText:"Quick flashcard app that allows you to create, delete, and edit flash cards.", infoImg: DashCardImg}}></DataBlock>
         <DataBlock propInput ={{title: "",infoTitle:"Portfolio", infoSubtitle:"TypeScript/Scss/React/HTML", infoText:"This portfolio was created from scratch using the skills I learned as a web developer from my summer internship.  The designs, logo, and code was all done by me.", infoImg: krLogo}}></DataBlock>
-        <DataBlock propInput ={{title: "Skills",infoTitle: null, infoSubtitle:"", infoText:"", infoImg: null}}></DataBlock>
+        <DataBlock propInput ={{title: "Skills",ignoreInfo: "", infoTitle: null, infoSubtitle:"", infoText:"", infoImg: null}}></DataBlock>
         <DataBlock propInput ={{title: "",infoTitle:"Languages:", infoSubtitle:"Java, Javascript, Python, and C, HTML/CSS", infoText:"", infoImg: ""}}></DataBlock>
         <DataBlock propInput ={{title: "",infoTitle:"Other Technologies:", infoSubtitle:"React, Redux, MongoDB, Node.js, Android Studios", infoText:"", infoImg: ""}}></DataBlock>
-        <DataBlock propInput ={{title: "Contact",infoTitle: "Click the button to contact me!", infoSubtitle:"", infoText:"", infoImg: null, other: <a href="mailto:ker108@pitt.edu" className="emailButton">Email</a>}}></DataBlock>
+        <DataBlock propInput ={{title: "Contact",infoTitle: null, infoSubtitle:"", infoText:"", infoImg: null, other: <a href="mailto:ker108@pitt.edu" className="emailButton"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/></svg></a>}}></DataBlock>
   </div>;
     return content;
 };
