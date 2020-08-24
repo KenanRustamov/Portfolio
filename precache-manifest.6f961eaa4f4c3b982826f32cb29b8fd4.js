74cc373065faf2bd3e27a9b402336a3be1a15911
Kenan Rustamov <kenanrustamov@gmail.com>
2020-08-24 13:26:35 -0400

    Updates

---
+++ precache-manifest.6f961eaa4f4c3b982826f32cb29b8fd4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ac5fd99d35be078dca2c1fc83afba3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8e63e1132110670a28f",
+    "url": "/static/css/main.2d5dfdd6.chunk.css"
+  },
+  {
+    "revision": "03e174a80c50dae5181d",
+    "url": "/static/js/2.13452fd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13452fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8e63e1132110670a28f",
+    "url": "/static/js/main.0c15f15c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "1b920709524d58ce263ef50942546d4f",
+    "url": "/static/media/BentleyPhoto.1b920709.jpeg"
+  },
+  {
+    "revision": "b71a8a628d505a0f8de673bd329df643",
+    "url": "/static/media/bnymellon.b71a8a62.png"
+  },
+  {
+    "revision": "31e405864f3048086e24e737080267f6",
+    "url": "/static/media/kenanRustamov.31e40586.jpg"
+  },
+  {
+    "revision": "edf161458dbd051fa3f65832b2992098",
+    "url": "/static/media/krLogo.edf16145.svg"
+  },
+  {
+    "revision": "bca6e0220c13d9c64682d7bd8eeba998",
+    "url": "/static/media/partify.bca6e022.png"
+  },
+  {
+    "revision": "cf227adb9c68eb291324f7d735de77e7",
+    "url": "/static/media/reactLogo.cf227adb.svg"
+  },
+  {
+    "revision": "8d2afbe971196d02d014c10f29e7fb75",
+    "url": "/static/media/snackcheck.8d2afbe9.png"
+  }
+]);
