8de800eddae2559fa93ac32df5c959220c42d10a
Kenan Rustamov <kenanrustamov@gmail.com>
2025-04-08 22:07:01 -0400

    looks much better now

--- src/components/DataBlocks.tsx
+++ src/components/DataBlocks.tsx
@@ -15,18 +15,19 @@ const DataBlocks = (props: any) => {
         <DataBlock 
             className="card" 
             id ={"1"} 
+            layoutType="intro"
             github={"https://github.com/KenanRustamov"} 
             link ={"https://www.linkedin.com/in/kenanrustamov/"} 
             instagram={"https://www.instagram.com/kenanrustamov1/"}
             resume={"/ComputerScienceResume.pdf"}
             propInput ={{infoTitle:"Kenan Rustamov", infoSubtitle:"Full Stack Developer", infoText:"I currently major in Computer Science with a minor in Chemistry at the University of Pittsburgh. I am a co-founder of a club for minorities in computing and fundraising chair of Oakland Outreach, a volunteer club. Currently, I also TA for Discrete Structures.", infoImg: kenanPhoto, alt: "Professionalish looking photo at the beach"}}></DataBlock>
         <div className="dataBlock card">
-            <DataBlock className="interior" id ={"2"} link ={"https://bnymellon.com"} propInput ={{title: "Work Experience",infoTitle:"BNY Mellon", infoSubtitle:"Full-Stack Software Engineering Intern", infoText: "Worked on a project to allow internal users to communicate with Fircosoft to verify large transactions utilizing test-driven development in an agile environment.  Specifically worked on creating CRUD operations for the project and database/project setup.", infoImg: bnymellon, alt: "image of bnymellon"}}></DataBlock>
-            <DataBlock className="interior" link= {"https://www.bentley.com/en"} propInput ={{title: "",infoTitle:"Bentley Systems", infoSubtitle:"Full-Stack Software Engineering Intern", infoText: "Created the ability for end users of the application to add and remove users from projects, persisted in the backend.", infoImg: BentleyPhoto, alt: "Yes, that is my favorite shirt"}}></DataBlock>
+            <DataBlock className="interior" id ={"2"} layoutType="standard" link ={"https://bnymellon.com"} propInput ={{title: "Work Experience",infoTitle:"BNY Mellon", infoSubtitle:"Full-Stack Software Engineering Intern", infoText: "Worked on a project to allow internal users to communicate with Fircosoft to verify large transactions utilizing test-driven development in an agile environment.  Specifically worked on creating CRUD operations for the project and database/project setup.", infoImg: bnymellon, alt: "image of bnymellon"}}></DataBlock>
+            <DataBlock className="interior" layoutType="standard" link= {"https://www.bentley.com/en"} propInput ={{infoTitle:"Bentley Systems", infoSubtitle:"Full-Stack Software Engineering Intern", infoText: "Created the ability for end users of the application to add and remove users from projects, persisted in the backend.", infoImg: BentleyPhoto, alt: "Yes, that is my favorite shirt"}}></DataBlock>
         </div>
         <div className="dataBlock card">
-            <DataBlock className="interior" id ={"3"} github={"https://github.com/KenanRustamov/Partify"} link ={"http://www.partify-us.com/"} propInput ={{title: "Projects",infoTitle:"Partify", infoSubtitle:"React TypeScript Scss", infoText:"Partify is an app that allows you to create a custom tailored playlist for any occasion based on the user and their friends' music tastes.", infoImg: partifyImg, alt: "Image of my partify application"}}></DataBlock>
-            <DataBlock className="interior" link ={"https://github.com/KenanRustamov/SnackCheck"} propInput ={{title: "",infoTitle:"SnackCheck", infoSubtitle:"JavaScript React-Native Expo", infoText:"SnackCheck allows users to find recipes based on dietary preferences such as veganism or allergies such as shellfish.", infoImg: SnackCheck, alt: "Image of my snackcheck application"}}></DataBlock>
+            <DataBlock className="interior" id ={"3"} layoutType="standard" github={"https://github.com/KenanRustamov/Partify"} link ={"http://www.partify-us.com/"} propInput ={{title: "Projects",infoTitle:"Partify", infoSubtitle:"React TypeScript Scss", infoText:"Partify is an app that allows you to create a custom tailored playlist for any occasion based on the user and their friends' music tastes.", infoImg: partifyImg, alt: "Image of my partify application"}}></DataBlock>
+            <DataBlock className="interior" layoutType="standard" link ={"https://github.com/KenanRustamov/SnackCheck"} propInput ={{infoTitle:"SnackCheck", infoSubtitle:"JavaScript React-Native Expo", infoText:"SnackCheck allows users to find recipes based on dietary preferences such as veganism or allergies such as shellfish.", infoImg: SnackCheck, alt: "Image of my snackcheck application"}}></DataBlock>
         </div>
   </div>;
     return content;
